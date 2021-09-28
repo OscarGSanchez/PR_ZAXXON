b8fa629f1c53e34e0dc82d57738b680d88389f8a
Oscar Garcia <garciasanchz.oscar@gmail.com>
2021-09-28 14:16:09 +0200

    Movimiento y camara

---
+++ PR_ZAXXON_GarciaOscar/Assets/Scripts/Camara.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    //Variables necesarias para la opción de suavizado
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y + 2, playerPosition.position.z - 10);
+    }
+}
