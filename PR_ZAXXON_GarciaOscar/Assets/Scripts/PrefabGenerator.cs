5f0dc67d3667942d5faa58e78b50cb5a88fc04d4
Oscar Garcia <garciasanchz.oscar@gmail.com>
2021-10-13 10:21:22 +0200

    Segundo obstáculo y desaparición de objetos
    He creado un segundo obstáculo que se desplaza en Y y he creado nuevas líneas para que de se destruyan y no se acumulen

--- PR_ZAXXON_GarciaOscar/Assets/Scripts/PrefabGenerator.cs
+++ PR_ZAXXON_GarciaOscar/Assets/Scripts/PrefabGenerator.cs
@@ -6,15 +6,18 @@ public class PrefabGenerator : MonoBehaviour
 {
 
     [SerializeField] GameObject MyPrefab;
+    [SerializeField] GameObject MyPrefabY;
     [SerializeField] Transform InitPos;
+    [SerializeField] Transform InitPosY;
 
     float intervalo;
 
     // Start is called before the first frame update
     void Start()
     {
-        intervalo = 1f;
+        intervalo = 0.5f;
         StartCoroutine("CrearObstaculos");
+        StartCoroutine("CrearObstaculosY");
 
     }
 
@@ -23,13 +26,28 @@ public class PrefabGenerator : MonoBehaviour
         while (true)
         {
            
-            Vector3 instPos = new Vector3(Random.Range(-20f, 20f), 0f, InitPos.position.z);
+            Vector3 instPos = new Vector3(Random.Range(-40f, 40f), 0f, InitPos.position.z);
             Instantiate(MyPrefab, instPos, Quaternion.identity);
             yield return new WaitForSeconds(intervalo);
 
         }
 
+
     }
 
+    IEnumerator CrearObstaculosY()
+    {
+
+        while (true)
+        {
+
+            Vector3 instPos = new Vector3(Random.Range(-40f, 40f), Random.Range(10f, 40f), InitPosY.position.y);
+            Instantiate(MyPrefabY, instPos, Quaternion.identity);
+            yield return new WaitForSeconds(intervalo);
+
+        }
+
+
+    }
 
 }
--- PR_ZAXXON_GarciaOscar/Assets/Scripts/PrefabMov.cs
+++ PR_ZAXXON_GarciaOscar/Assets/Scripts/PrefabMov.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PrefabMov : MonoBehaviour
 {
 
-    [SerializeField] float speed = 10f;
+    [SerializeField] float speed = 100f;
 
     // Start is called before the first frame update
     void Update()
@@ -13,7 +13,23 @@ public class PrefabMov : MonoBehaviour
 
         transform.Translate(Vector3.back * Time.deltaTime * speed);
 
+        float posZ = transform.position.z;
+
+        if (posZ < -60)
+        {
+            Destroy(gameObject);
+        }
 
     }
+
+    private void Start()
+    {
+
+        speed = 50f;
+
+
+    }
+
+
 
 }
