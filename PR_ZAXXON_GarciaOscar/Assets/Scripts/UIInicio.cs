7f3149965d56e3473642bbfd640d9913a2a97737
Carlos <garciasanchz.oscar@gmail.com>
2021-11-10 10:22:19 +0100

    Interacción entre pantallas
    He programado los botones para que se muevan entre pantallas. Solo falta la pantalla de muerte cuando te chocas.

--- PR_ZAXXON_GarciaOscar/Assets/Scripts/UIInicio.cs
+++ PR_ZAXXON_GarciaOscar/Assets/Scripts/UIInicio.cs
@@ -1,30 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class UIButtons : MonoBehaviour
+public class UIInicio : MonoBehaviour
 {
-
-    
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetButtonDown("Start"))
-            
-        {
-
-            SceneManager.LoadScene(1);
-
-        }
+        
     }
+
+    public void LoadScene(int Inicio) 
+    
+    {
+
+        SceneManager.LoadScene(Inicio);
+
+    }
+
+
+
+
 }
